b7d36d665dc191361aabccded20f56b40a073fcc
SirAbody <0507849977aaa@gmail.com>
2025-10-16 18:03:49 +0300

    Fix JSX structure - add missing closing div

--- client/src/pages/DirectMessages.js
+++ client/src/pages/DirectMessages.js
@@ -1709,9 +1709,10 @@ function DirectMessages() {
           )
         )}
       </div>
+    </div>
 
-      {/* Add Friend Modal */}
-      {showAddFriend && (
+    {/* Add Friend Modal */}
+    {showAddFriend && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
           <div className="bg-black/90 border border-primary-900/30 rounded-lg p-6 w-96">
             <div className="flex justify-between items-center mb-4">
